689099a92a67510936381f3aadb3d9d0588ce886
Bradoshado <bradoshado@gmail.com>
2025-05-13 12:06:16 +0900

    add grid for CreatePost and SuggestedFollowers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+import CreatePost from "@/components/CreatePost";
+
 export default function Home() {
   return (
-    <div className="m-3">
-      <h1>Home Page</h1>
+    <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
+      <div className="lg:col-span-6">
+        <CreatePost />
+      </div>
+
+      <div className="hidden lg:block lg:col-span-4 sticky top-20">
+        Suggested People To Follow
+      </div>
     </div>
   );
 }
