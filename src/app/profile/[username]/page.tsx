6bdf01dcbdd5455db28b43ab9a0659f80d12519d
Bradoshado <bradoshado@gmail.com>
2025-05-21 23:24:53 +0900

    ProfilePageClient, page, and not-found components

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -1,7 +1,47 @@
-import React from "react";
+"use server";
 
-function page() {
-  return <div>user page</div>;
+import {
+  getProfileByUsername,
+  getUserLikedPosts,
+  getUserPosts,
+  isFollowing,
+} from "@/actions/profile";
+import { notFound } from "next/navigation";
+import ProfilePageClient from "./ProfilePageClient";
+
+export async function generateMetaData({
+  params,
+}: {
+  params: { username: string };
+}) {
+  const user = await getProfileByUsername(params.username);
+  if (!user) return;
+
+  return {
+    title: `${user.name ?? user.username}`,
+    description: user.bio || `Take a look at ${user.username}'s profile!`,
+  };
 }
 
-export default page;
+async function ProfilePageServer({ params }: { params: { username: string } }) {
+  const user = await getProfileByUsername(params.username);
+
+  if (!user) notFound();
+
+  const [posts, likedPosts, isCurrentUserFollowing] = await Promise.all([
+    getUserPosts(user.id),
+    getUserLikedPosts(user.id),
+    isFollowing(user.id),
+  ]);
+
+  return (
+    <ProfilePageClient
+      user={user}
+      posts={posts}
+      likedPosts={likedPosts}
+      isFollowing={isCurrentUserFollowing}
+    />
+  );
+}
+
+export default ProfilePageServer;
