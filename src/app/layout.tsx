0640157aa869a9b97c7e3193cb7c6e034759dd48
bennord <bennord@gmail.com>
2025-05-26 14:39:02 +0900

    better welcome logic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -45,21 +45,18 @@ async function AuthComponent({
             <Navbar />
             <main className="py-6">
               <div className="max-w-7xl mx-auto px-4">
-                <div className="grid grid-cols-1 lg:grid-cols-12 gap-6">
-                  <div className="hidden lg:block lg:col-span-3">
-                    <Sidebar />
-                  </div>
-
-                  {user ? (
-                    <></>
-                  ) : (
-                    <div className="col-span-6 max-w-2xl mx-auto">
-                      <WelcomeCard />
+                {user ? (
+                  <div className="grid grid-cols-1 lg:grid-cols-12 gap-6">
+                    <div className="hidden lg:block lg:col-span-3">
+                      <Sidebar />
+                      <div className="lg:col-span-9">{children}</div>
                     </div>
-                  )}
-
-                  <div className="lg:col-span-9">{children}</div>
-                </div>
+                  </div>
+                ) : (
+                  <div className="lg:max-w-2xl mx-auto">
+                    <WelcomeCard />
+                  </div>
+                )}
               </div>
             </main>
           </div>
